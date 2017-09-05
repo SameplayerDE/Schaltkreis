90becae124eeb891f31177581eb126a0f199f275
SameplayerDE <obekar22@gmail.com>
2017-09-05 20:49:58 +0200

    Added new file

--- frame.cpp
+++ frame.cpp
@@ -16,6 +16,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         case WM_DESTROY:
             PostQuitMessage(0);
             break;
+
         default:
             return DefWindowProc(hwnd, msg, wParam, lParam);
     }
